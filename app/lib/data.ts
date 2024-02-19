a13d6835c3241cf69ff6f5f7ec0096ec1a6b645c
Maria A. Valencia <fafevalo99@gmail.com>
2024-02-19 16:02:00 -0500

    hola

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -26,7 +26,8 @@ export async function fetchRevenue() {
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.'); 
+    console.log('Data fetch completed after 3 seconds.');
+    
 
     return data.rows;
   } catch (error) {
@@ -169,6 +170,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+    console.log(invoice);
 
     return invoice[0];
   } catch (error) {
